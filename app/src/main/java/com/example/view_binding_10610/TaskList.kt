8759090112b648e7ca00c71893affee547d50b4a
katsuronim <rereandreas9@gmail.com>
2022-09-12 22:54:20 +0700

    Ini commit Guided 1 Modul 4 (View Binding)

---
+++ app/src/main/java/com/example/view_binding_10610/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10610
+
+object TaskList {
+    val taskList = listOf<Task> (
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencair Primo buat Gacha Mama Yaemiko tercinta / Keqing wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "Dimana mama Ei?", "Buat adu mekanik showcase Ayaka pyro"),
+        Task("Chapter 5", "Upload ke Tiktok", "Buat pamer ke view Showcase Ayaka Pyro C6 + Bennet C6")
+    )
+}
